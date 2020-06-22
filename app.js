96e66c93d59ef1f15fe78656b60b97aceefd68e7
Anirban Das <anirbandas0206@gmail.com>
2020-06-22 14:05:57 +0530

    Add files via upload
    Created a new function in app.js  view_in_iframe()

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-
 // Just puts the default value in the text areas
 function default_text()
 {
+     // putting defaut values in the text area
     document.getElementById("Html").defaultValue = "<!DOCTYPE html>\n<h1>Hello World !</h1>";
     document.getElementById("Css").defaultValue = "body{ background: orange; }";
-    var viewer = document.getElementById("Viewer").contentWindow.document;
-    viewer.open();
-      viewer.writeln(
-        document.getElementById("Html").value +
-          "<style>" + document.getElementById("Css").value + "</style>"
-      );
-      viewer.close();
+    document.getElementById("Js").defaultValue = "";
+    
+    //calling the function to show the default values in iframe.. 
+    view_in_iframe();
+    
 }
 
-// Every time when key is released this function will be trigerred 
-function compile() {
-    
-    var html = document.getElementById("Html");
+// this function is used to print the html css and js inside iframe..
+function view_in_iframe(){
 
-    var css = document.getElementById("Css");
-
-    var js = document.getElementById("Js");
-
-    var viewer = document.getElementById("Viewer").contentWindow.document;
+  var html = document.getElementById("Html");
+  var css = document.getElementById("Css");
+  var js = document.getElementById("Js");
+  var viewer = document.getElementById("Viewer").contentWindow.document;
   
-    document.body.onkeyup = function() {
-      viewer.open();
+  // printing inside iframe .. binding html css js together.. 
+  viewer.open();
       viewer.writeln(
         html.value +
           "<style>" + css.value + "</style>" + "<script>" +js.value +"</script>"
       );
       viewer.close();
-    };
-  }
-  default_text(); 
-  compile();
+} 
+
+//calling the functions
+default_text();
+// Every time when key is released this function will be trigerred 
+   document.body.onkeyup = function()
+   {
+       view_in_iframe();
+   };
+ 

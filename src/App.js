5328966d9add3e4ee11fd78cde333eef65ad8451
Shrabana Biswas <49238570+shrabana99@users.noreply.github.com>
2020-06-25 20:40:18 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import { Body } from './components/Body';
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+    	<Navbar/>
+    	<Body/>
+    </div>
+  );
+}
+
+export default App;
+
+

f14a4e585521e59e2a36d2bd4911752792ed0e2b
Shrabana Biswas <49238570+shrabana99@users.noreply.github.com>
2020-07-03 01:01:12 +0530

    Added contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,25 +6,40 @@ const Contact = () =>{
         <div className="container">
            <h4>Contributors</h4>
          <ul class="collapsible popout">   
-    <li>
-      <div class="collapsible-header"><i class="large material-icons ">face</i>Shrabana Biswas</div>
-      <div class="collapsible-body"><span>
-          <p>
-          <a href = "https://in.linkedin.com/in/shrabana-biswas-486ab9167" target="_blank">
-                  linkedin </a>
-          </p>
-          <p>
-          <a href = "https://github.com/shrabana99" target="_blank">
-                  linkedin </a>
-          </p>
+            <li>
+              <div class="collapsible-header"><i class="large material-icons ">face</i>Shrabana Biswas</div>
+              <div class="collapsible-body"><span>
+                <p> Final year undergraduate, passionate about software and web development. </p>
+                  <p>Connect me on  
+                  <a href = "https://in.linkedin.com/in/shrabana-biswas-486ab9167" target="_blank">
+                          LinkedIn </a>
+                  </p>
+                  <p>View my other works on  
+                  <a href = "https://github.com/shrabana99" target="_blank">
+                          Github </a>
+                  </p>
 
-      </span></div>
-    </li>
-    <li>
-      <div class="collapsible-header"><i class="large material-icons">face</i>Anirban Das</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span></div>
-    </li>
-  </ul>
+              </span></div>
+            </li>
+            <li>
+              <div class="collapsible-header"><i class="large material-icons ">face</i>Anirban Das</div>
+              <div class="collapsible-body"><span>
+                <p> B.Tech undergrad with passion about learning by doing
+                and a zeal to learn new things and experiences. 
+
+                 </p>
+                  <p>Connect me on  
+                  <a href = "https://in.linkedin.com/in/shrabana-biswas-486ab9167" target="_blank">
+                          LinkedIn </a>
+                  </p>
+                  <p>View my other works on  
+                  <a href = "https://github.com/shrabana99" target="_blank">
+                          Github </a>
+                  </p>
+
+              </span></div>
+            </li>
+          </ul>
      </div>
 
   );
